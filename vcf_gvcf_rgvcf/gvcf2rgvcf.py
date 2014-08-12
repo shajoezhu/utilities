6e8614b8636193ce5c15836d264e1ae2258a5d97
Joe Zhu <joe.zhu@well.ox.ac.uk>
2014-08-12 17:42:58 +0000

    debug for gvcf to rgvcf
    git-svn-id: svn+ssh://florence/Net/fs1/groups/bioinformatics/svn/repos/branches/optimising_count@3024 09689bc1-c481-4e07-9956-135ddad18f52

--- vcf_gvcf_rgvcf/gvcf2rgvcf.py
+++ vcf_gvcf_rgvcf/gvcf2rgvcf.py
@@ -3,13 +3,32 @@
 import sys
 
 def convert_gvcf_to_rgvcf_line ( tmp_line, next_base_ ):
+    #print tmp_line
     line_split = tmp_line.split()
     seg_start = int(line_split[1])
-    seg_end   = int(line_split[7].strip("END=").strip(";"))
+    END_index = line_split[7].find ("END=")
+    
+    
+    tmp_info_field = line_split[7].strip(line_split[7][0:END_index]).strip("END=")
+    semi_col_index = tmp_info_field.find (";")    
+    seg_end = int(tmp_info_field[0:semi_col_index])
     line_split[1] = `seg_end`
     line_split[7] = "END="+`next_base_-1`+";"
     lineout = "\t".join(line_split) + "\n"
+    #print seg_end, next_base_, seg_end < (next_base_-1)
     return lineout if seg_end < next_base_-1 else ""
+
+#def fullfill_rgvcf_line ( tmp_line, next_base_ ):
+    #line_split = tmp_line.split()
+    ##seg_start = int(line_split[1])
+    ##seg_end   = int(line_split[7].strip("END=").strip(";"))
+    #print tmp_line
+    #line_split[1] = `int(line_split[1])+1`
+    #line_split[7] = "END="+`next_base_`+";"
+    #lineout = "\t".join(line_split) + "\n"
+    #return lineout 
+
+
 
 def convert_gvcf_to_rgvcf_file ( gvcf_name, rgvcf_name, end_base ):
     gvcf = open ( gvcf_name, 'r' )
@@ -19,7 +38,8 @@ def convert_gvcf_to_rgvcf_file ( gvcf_name, rgvcf_name, end_base ):
         if line_i == 0:
             line = "##fileformat=RGVCF\n"
         end_of_seq_bool = False
-        if line.find ("END") > 0: # if END is not find, line.find returns "-1"
+        #print line
+        if line.find ("END=") > 0: # if END is not find, line.find returns "-1"
             try:
                 next_base = int( gvcfLines[line_i+1].split()[1] )
                 if next_base > end_base:
@@ -29,10 +49,12 @@ def convert_gvcf_to_rgvcf_file ( gvcf_name, rgvcf_name, end_base ):
                 next_base = end_base + 1
                 end_of_seq_bool = True
             line = convert_gvcf_to_rgvcf_line ( line, next_base )
-        rgvcf.write(line)            
+        rgvcf.write(line)
         if end_of_seq_bool:
-            #print line
+            if line.find ("END=") < 0:
+                print line
             break;
+
     gvcf.close()
     rgvcf.close()
 
