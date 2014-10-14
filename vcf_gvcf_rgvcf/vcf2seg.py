093c64cce34c65d385b6f7e39fafb93d9abb0536
shajoezhu <sha.joe.zhu@gmail.com>
2014-10-14 13:07:10 +0100

    save progress

--- vcf_gvcf_rgvcf/vcf2seg.py
+++ vcf_gvcf_rgvcf/vcf2seg.py
@@ -2,7 +2,6 @@
 
 import sys
 import os
-#import os.infile_name
 
 class inputs:
     def check_next_argvi(self):
@@ -15,16 +14,15 @@ class inputs:
     def choose_suffix(self):
         if   ".rgvcf" in self.infile_name: 
             self.filetype = self.RGVCF
-            self.outfile_name = self.infile_name.strip(".rgvcf") + ".seg"
+            self.outfile_name = self.infile_name.replace(".rgvcf", ".seg")
         elif  ".gvcf" in self.infile_name: 
             self.filetype = self.GVCF
-            self.outfile_name = self.infile_name.strip(".gvcf") + ".seg"
+            self.outfile_name = self.infile_name.replace(".gvcf", ".seg")
         elif   ".vcf" in self.infile_name: 
             self.filetype = self.VCF
-            self.outfile_name = self.infile_name.strip(".vcf") + ".seg"
+            self.outfile_name = self.infile_name.replace(".vcf", ".seg")
         else: print "File type can not be determined, suffix ends with '.vcf', '.gvcf' or '.rgvcf'."
-        #print self.outfile_name, self.filetype
-        #print self.filetype # DEBUG
+        print self.outfile_name, self.filetype # DEBUG
         
         
     def __init__(self, argv):
@@ -45,27 +43,36 @@ class inputs:
                 self.seqlen = int( self.argv[self.argi] )
             else:
                 print "Invalid flag: ", self.argv[self.argi] 
-                print "-i FILENAME -seqlen INT"
+                print_usage()
                 sys.exit()
             self.argi = self.argi + 1                                    
 
 
 class something2seg:
     def __init__(self, infile_type, infile_name, outfile_name, seqlen):
+        self.outfile_name = outfile_name
+        self.seqlen = seqlen
         [self.VCF, self.GVCF, self.RGVCF] = range ( 3 ) # enum vcf, gvcf rgvcf 
         self.infile = open ( infile_name, 'r' )
-        infile_Lines = self.infile.readlines()
+        self.infile_Lines = self.infile.readlines()
         self.infile.close()
 
-        self.outfile = open ( outfile_name, 'w' ) 
-        for line_index, line in enumerate(infile_Lines):
-            
+    def core(self):
+        self.pre_process();
+        self.outfile = open ( self.outfile_name, 'w' ) 
+        self.outfile.close()
+        pass
+        
+    def pre_process ( self ):
+        # Pre-process, skip all the comments lines
+        for line_index, line in enumerate( self.infile_Lines ):
             if line.find ( "##" ) == 0: # skipping the comments
                 continue
-            print line
-
-        self.outfile.close()
-        
+            elif line.find ( "#" ) == 0: #CHROM  POS     ID      REF     ALT     QUAL    FILTER  INFO    FORMAT  NA00001 NA00002 NA00003
+                pass
+            print line        
+    
+    
 def vcf2seg( infile_name, outfile_name ):
     pass
     
@@ -78,19 +85,20 @@ def rgvcf2seg( infile_name, outfile_name ):
     pass
 
 
+def print_usage():
+    print "USAGE:"
+    print "    ./vcf2seg -i infile.vcf -seqlen 200000"
+    sys.exit()
+
+
 if __name__ == "__main__":
     
-    if sys.argv[1] == "-help":
-        print "./vcf2seg -i infile.vcf -seqlen 200000"
-        sys.exit()
+    if len(sys.argv) == 1:
+        print_usage()
+    elif sys.argv[1] == "-help":
+        print_usage()
     
     myinput = inputs( sys.argv )
     print myinput.infile_name
-    something2seg(myinput.filetype, myinput.infile_name, myinput.outfile_name, myinput.seqlen)    
-    #infile_name = sys.argv[1]
-    #seqlen = int ( sys.argv[2] )
-    #file_name_prefix = infile_name.strip(".gvcf")
-    #print file_name_prefix
-    #outfile_name = file_name_prefix + ".rgvcf"
-    #print outfile_name
-    #convert_gvcf_to_rgvcf_file ( infile_name, outfile_name , seqlen )
+    myprocess = something2seg(myinput.filetype, myinput.infile_name, myinput.outfile_name, myinput.seqlen)    
+    myprocess.core()
