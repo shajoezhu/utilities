e16c30a065be661e763b6111e40f94ca8f01b374
shajoezhu <sha.joe.zhu@gmail.com>
2014-10-14 18:02:47 +0100

    vcf2seg is working

--- vcf_gvcf_rgvcf/README.md
+++ vcf_gvcf_rgvcf/README.md
@@ -1,11 +1,14 @@
+```bash
 ../pop_struct.py sim-1 2 1
-
 mv sim-1Samples2msdata1.seg sim-1.seg
+```
 
-vcf2seg.py -i sim-1Samples2msdata1.vcf -seqlen 30000000
-diff 1Samples2msdata1.seg sim-1.seg
+```bash
+./vcf2seg.py -i sim-1Samples2msdata1.vcf -seqlen 30000000
+diff sim-1Samples2msdata1.seg sim-1.seg
+```
 
-``~bash
-vcf2seg.py -i sim-1Samples2msdata1.gvcf -seqlen 30000000
+```bash
+./vcf2seg.py -i sim-1Samples2msdata1.gvcf -seqlen 30000000
 diff 1Samples2msdata1.seg sim-1.seg
-``
+```
--- vcf_gvcf_rgvcf/vcf2seg.py
+++ vcf_gvcf_rgvcf/vcf2seg.py
@@ -5,12 +5,11 @@ import os
 
 class inputs:
     def check_next_argvi(self):
-        self.argi = self.argi + 1
+        self.argi += 1
         if self.argi == self.argc:
             print "Missing argument after flag: " + self.argv[self.argi-1]
             sys.exit()
-            
-    
+                
     def choose_suffix(self):
         if   ".rgvcf" in self.infile_name: 
             self.filetype = self.RGVCF
@@ -23,8 +22,7 @@ class inputs:
             self.outfile_name = self.infile_name.replace(".vcf", ".seg")
         else: print "File type can not be determined, suffix ends with '.vcf', '.gvcf' or '.rgvcf'."
         print self.outfile_name, self.filetype # DEBUG
-        
-        
+                
     def __init__(self, argv):
         [self.VCF, self.GVCF, self.RGVCF] = range ( 3 ) # enum vcf, gvcf rgvcf 
         self.argv = argv
@@ -45,46 +43,104 @@ class inputs:
                 print "Invalid flag: ", self.argv[self.argi] 
                 print_usage()
                 sys.exit()
-            self.argi = self.argi + 1                                    
+            self.argi += 1
 
 
 class something2seg:
     def __init__(self, infile_type, infile_name, outfile_name, seqlen):
+        self.infile_type = infile_type
         self.outfile_name = outfile_name
         self.seqlen = seqlen
         [self.VCF, self.GVCF, self.RGVCF] = range ( 3 ) # enum vcf, gvcf rgvcf 
         self.infile = open ( infile_name, 'r' )
         self.infile_Lines = self.infile.readlines()
         self.infile.close()
+        self.variant_pos = 1
+        self.genetic_break = "F"
 
+    def find_next_position(self):
+        self.next_position = int( self.infile_Lines [self.line_index + 1].split("\t")[1] ) if self.line_index + 1 < self.infile_size else self.seqlen
+            
     def core(self):
         self.pre_process();
         self.outfile = open ( self.outfile_name, 'w' ) 
-        self.outfile.close()
-        pass
         
+        self.infile_size = len ( self.infile_Lines )
+        #print self.line_index, infile_size  # DEBUG
+        # Initialize the first line of vcf file, start from position 1
+
+        if self.infile_type == self.VCF:
+            line_split = self.line.strip().split("\t")
+            self.chrom = line_split [0] 
+            self.variant = ""
+            for field in range( 9, len(line_split)):                        
+                self.variant += ".."
+            self.line_index -= 1
+            self.find_next_position ()
+            self.line_index += 1
+            self.seg_len = self.next_position - self.variant_pos
+            self.seg_status = "T"
+            current_line = `self.variant_pos`      + "\t" + \
+                           `self.seg_len`  + "\t" + \
+                           self.seg_status + "\t" + \
+                           self.genetic_break + "\t" + \
+                           self.chrom      + "\t" + \
+                           self.variant + "\n"
+            self.outfile.write ( current_line )
+        while self.line_index < self.infile_size:
+            self.extract_infile_line ( self.infile_Lines [self.line_index] )
+            self.write_seg_line()
+            self.line_index += 1
+            pass
+        
+        self.outfile.close()
+        
+
     def pre_process ( self ):
-        # Pre-process, skip all the comments lines
-        for line_index, line in enumerate( self.infile_Lines ):
-            if line.find ( "##" ) == 0: # skipping the comments
+        # Pre-process, skip all the comments lines,
+        # Extract the number of taxa ( 2*number_of_sample ) at the last line
+        for self.line_index, self.line in enumerate( self.infile_Lines ):
+            if self.line.find ( "##" ) == 0: # skipping the comments
                 continue
-            elif line.find ( "#" ) == 0: #CHROM  POS     ID      REF     ALT     QUAL    FILTER  INFO    FORMAT  NA00001 NA00002 NA00003
-                pass
-            print line        
-    
-    
-def vcf2seg( infile_name, outfile_name ):
-    pass
-    
+            elif self.line.find ( "#" ) == 0: #CHROM  POS     ID      REF     ALT     QUAL    FILTER  INFO    FORMAT  NA00001 NA00002 NA00003
+                line_split = self.line.strip().split("\t")
+                self.taxa = 2 * ( len(line_split) - 9 ) # where ( len(line_split) - 9 ) computes the number of the samples
+            else:
+                break; # stops at the first line
+        print "number of sample is ",self.taxa # DEBUG
 
-def gvcf2seg( infile_name, outfile_name ):
-    pass
-    
-
-def rgvcf2seg( infile_name, outfile_name ):
-    pass
-
-
+    def extract_infile_line( self, line ):
+        line_split = line.strip().split("\t")
+        self.chrom = line_split [0] 
+        self.variant_pos = int(line_split [1])
+        self.variant = ""
+        for field in range( 9, len(line_split)):                        
+            current_field = line_split[field]
+            #print current_field[0], current_field[2] # DEBUG
+            self.variant += current_field[0] if ( current_field[0] == "." or current_field[0] == "0") else `1`
+            self.variant += current_field[2] if ( current_field[2] == "." or current_field[2] == "0") else `1`
+        #print self.variant # DEBUG
+        
+    def write_seg_line (self):
+        if  self.infile_type == self.VCF:
+            self.find_next_position()
+            self.seg_len = self.next_position - self.variant_pos
+            self.seg_status = "T"
+            current_line = `self.variant_pos`      + "\t" + \
+                           `self.seg_len`  + "\t" + \
+                           self.seg_status + "\t" + \
+                           self.genetic_break + "\t" + \
+                           self.chrom      + "\t" + \
+                           self.variant + "\n"
+            self.outfile.write ( current_line )
+            self.variant = self.variant
+            self.pos = self.variant_pos
+        elif self.infile_type == self.GVCF:
+            pass
+        else:
+            pass
+            
+            
 def print_usage():
     print "USAGE:"
     print "    ./vcf2seg -i infile.vcf -seqlen 200000"
@@ -99,6 +155,5 @@ if __name__ == "__main__":
         print_usage()
     
     myinput = inputs( sys.argv )
-    print myinput.infile_name
     myprocess = something2seg(myinput.filetype, myinput.infile_name, myinput.outfile_name, myinput.seqlen)    
     myprocess.core()
